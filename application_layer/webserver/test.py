3ae9a84ef14f31e7b46a0c93deb7735091c5073a
Razvan Rotundu <raz.rotundu@gmail.com>
2024-05-30 10:24:31 -0400

    Restart

---
+++ application_layer/webserver/test.py
@@ -0,0 +1,5 @@
+
+
+    
+if __name__ == '__main__':
+    print(get_filename("GET /index.html HTTP/1.1"))
---
+++ application_layer/webserver/webserver.py
@@ -0,0 +1,6 @@
+# RRotundu 2024
+# Basic webserver that returns html files
+
+
+if __name__ == '__main__':
+    print("Hello World")
