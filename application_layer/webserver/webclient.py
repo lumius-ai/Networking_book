328c314ffa466dbccd528b73588385cb12a481e0
Razvan Rotundu <raz.rotundu@gmail.com>
2024-05-30 14:09:09 -0400

    connect weclient to target server

--- application_layer/webserver/webclient.py
+++ application_layer/webserver/webclient.py
@@ -1,10 +1,19 @@
 # RRotundu 2024
 # A simple web client that makes TCP connection to server and does GET, POST, PUT and DELETE
+import sys
+import socket
 
 # Start the webclient
-def webclient_start(address="127.0.0.1", port=5501):
+def webclient_start(server_address="127.0.0.1", server_port=5500):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_address, server_port))
+    
+    return 0
 
 if __name__ == "__main__":
-    if(len(sys.argv) == 2):
-        address = 
-    return 0
+    if(len(sys.argv) == 3):
+        address = sys.argv[1]
+        port = sys.argv[2]
+        print(f"Using {address} and {port} as client specs")
+    print("Using LOCALHOST and PORT=5500 as client specs")
+    webclient_start()
